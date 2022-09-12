663cb191461b649bcb65b8ce0a7f306e1ba3cc5b
alievqara <alievqara@outlook.com>
2022-09-12 16:12:52 +0400

    update

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ function check(){
     var z =parseFloat(document.getElementById('faiz').value);
 
     if(x>= 100 && x <= 100000 && y>=3 && y <=48 && z<=20 && z>=16){
-        // var ayliq=(x/y)+(x*(z/100));
         var faiz = (y/12)*z
         var count = x+((x/100)*faiz)
         var mounth = count / y
